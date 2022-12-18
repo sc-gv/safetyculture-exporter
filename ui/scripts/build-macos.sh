6df47d20602acaa6632dcad37882a6971ddf776e
Gaurav Verma <gaurav.verma@safetyculture.io>
2022-12-18 17:28:18 +1100

    fixed path for ui build

--- ui/scripts/build-macos.sh
+++ ui/scripts/build-macos.sh
@@ -5,13 +5,13 @@ echo "building on AMD64"
 wails build -platform darwin/amd64 -clean
 
 echo "Zipping Package"
-ditto -c -k --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-amd64.zip
+ditto -c -k --keepParent ./ui/build/bin/safetyculture-exporter.app ./exporter-darwin-amd64.zip
 echo "Cleaning up"
-rm -rf build/bin/SafetyCulture Exporter.app
+rm -rf ./ui/build/bin/SafetyCulture Exporter.app
 
 echo "building on ARM64"
 wails build -platform darwin/arm64 -clean
 echo "Zipping Package"
-ditto -c -k --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-arm64.zip
+ditto -c -k --keepParent ./ui/build/bin/safetyculture-exporter.app ./exporter-darwin-arm64.zip
 echo "Cleaning up"
-rm -rf ./build/bin/SafetyCulture Exporter.app
+rm -rf ./ui/build/bin/SafetyCulture Exporter.app
